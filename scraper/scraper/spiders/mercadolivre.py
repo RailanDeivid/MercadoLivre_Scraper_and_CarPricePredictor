9d83453730c3f032d9b92f0add46eb1fcec40936
Railan Deivid <contato.railandeivid@gmail.com>
2024-06-22 10:59:34 -0300

    update

--- scraper/scraper/spiders/mercadolivre.py
+++ scraper/scraper/spiders/mercadolivre.py
@@ -4,7 +4,7 @@ class MercadolivreSpider(scrapy.Spider):
     name = "mercadolivre"
     start_urls = ["https://lista.mercadolivre.com.br/veiculos"]
     page_count = 1
-    max_pages = 50
+    max_pages = 45
 
     def parse(self, response):
         self.log(f'Parsing page {self.page_count}')
