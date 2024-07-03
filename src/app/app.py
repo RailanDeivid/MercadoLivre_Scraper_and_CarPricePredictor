c48079848d552eeff96d641ee84b564b387fc334
Railan Deivid <contato.railandeivid@gmail.com>
2024-07-03 18:20:39 -0300

    update

--- src/app/app.py
+++ src/app/app.py
@@ -8,10 +8,9 @@ import numpy as np
 
 
 # Carregando o modelo pré-treinado
-model_path = os.path.abspath(os.path.join(os.getcwd(), '..','..', 'models', 'previsao_precos_veiculos_20240702.joblib'))
-model = jb.load(model_path)
+# model_path = os.path.abspath(os.path.join(os.getcwd(), '..','..', 'models', 'previsao_precos_veiculos_20240702.joblib'))
+model = jb.load('previsao_precos_veiculos_20240702.joblib')
 model = model['model']
-
 
 
 # Carregando dados adicionais
