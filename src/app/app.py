169fe48dc373fb43d4710bfd8b8f3520d3be9a0b
Railan Deivid <contato.railandeivid@gmail.com>
2024-07-04 15:08:37 -0300

    update

--- src/app/app.py
+++ src/app/app.py
@@ -4,57 +4,83 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+# Função para formatar moeda
+def formatar_moeda_brl(valor):
+    valor_formatado = f"R$ {valor:,.2f}".replace('.', '#').replace(',', '.').replace('#', ',')
+    return valor_formatado
+
+# Caminho dos arquivos
 path = os.path.dirname(__file__)
 my_model = path+'/data/previsao_precos_veiculos_20240702.joblib'
+my_data = path+'/data/data_tratados.parquet'
 
 # Carregando o modelo
-#model = joblib.load('./data/previsao_precos_veiculos_20240702.joblib')
 model = joblib.load(my_model)
 model = model['model']
 
-path = os.path.dirname(__file__)
-my_data = path+'/data/data_tratados.parquet'
 # Carregando dados adicionais
 df_dados_adicionais = pd.read_parquet(my_data)
 
 # Extraindo as opções únicas para o estado (UF)
 ufs = df_dados_adicionais['uf'].unique().tolist()
 
+# setando titulo
+st.markdown("""
+    <style>
+        .rounded-title {
+            background-color: #b89100;
+            border-radius: 10px;
+            padding: 10px;
+            text-align: center;
+            font-size: 50px;
+        }
+    </style>
+    """, unsafe_allow_html=True)
+st.markdown("<h1 class='rounded-title'>Previsão de Preço de Carros</h1>", unsafe_allow_html=True)
+
+# setando titulos e informações
+st.markdown("<h1 style='font-size: 30px;'>Informações do Modelo</h1>", unsafe_allow_html=True)
+st.markdown("""
+Esta aplicação utiliza um modelo de machine learning para prever o preço de vendas de carros com base nas características inseridas. 
+            
+O modelo foi treinado com dados de anuncios de vendas de veiculos do site MercadoLivre.
+""")
+st.markdown("<h2 style='font-size: 25px; text-align: center;'>Insira as características do carro</h2>", unsafe_allow_html=True)
+
 # Definindo a interface do usuário
-st.title("Previsão de Preço de Carros")
-st.markdown("Insira as características do carro:")
+col1, col2 = st.columns(2)
 
-# Entradas do usuário na ordem especificada
-ano = st.number_input('Ano', min_value=1994, max_value=2025, step=1, format="%d")
-motor = df_dados_adicionais['motor'].unique().tolist()
-# Filtrando dados com base no ano selecionado
-df_filtro_por_ano = df_dados_adicionais[df_dados_adicionais['ano'] == ano]
-marcas = df_filtro_por_ano['marca'].unique().tolist()
+with col1:
+    ano = st.number_input('Ano', min_value=1994, max_value=2025, step=1, format="%d")
+    motor = df_dados_adicionais['motor'].unique().tolist()
+    # Filtrando dados com base no ano selecionado
+    df_filtro_por_ano = df_dados_adicionais[df_dados_adicionais['ano'] == ano]
+    marcas = df_filtro_por_ano['marca'].unique().tolist()
 
-# Selecionando a marca
-if marcas:
-    marca_selecionada = st.selectbox('Marca', marcas)
-    # Filtrando dados com base na marca selecionada
-    df_filtro_por_marca = df_filtro_por_ano[df_filtro_por_ano['marca'] == marca_selecionada]
-    modelos = df_filtro_por_marca['modelo'].unique().tolist()
-else:
-    marca_selecionada = st.selectbox('Marca', [])
-    modelos = []
+    # Selecionando a marca
+    if marcas:
+        marca_selecionada = st.selectbox('Marca', marcas)
+        # Filtrando dados com base na marca selecionada
+        df_filtro_por_marca = df_filtro_por_ano[df_filtro_por_ano['marca'] == marca_selecionada]
+        modelos = df_filtro_por_marca['modelo'].unique().tolist()
+    else:
+        marca_selecionada = st.selectbox('Marca', [])
+        modelos = []
 
-# Selecionando o modelo
-modelo = st.selectbox('Modelo', modelos)
-# Selecionando UF 
-UF = st.selectbox('Estado (UF)', ufs)
-# Selecionando o motor
-motor = st.selectbox('motor', motor)
+    # Selecionando o modelo
+    modelo = st.selectbox('Modelo', modelos)
+    # Selecionando UF 
+    UF = st.selectbox('Estado (UF)', ufs)
+    # Selecionando o motor
+    motor = st.selectbox('motor', motor)
+    transmissao = st.selectbox('Tipo de Transmissão', ['Automatica', 'Manual', 'Semiautomatica'])
 
-KM = st.number_input('Quilometragem (KM)', min_value=0.0, format="%f")
-vidros_eletricos = st.selectbox('Vidros Elétricos', [0, 1], format_func=lambda x: 'Sim' if x == 1 else 'Não')
-ar_condicionado = st.selectbox('Ar Condicionado', [0, 1], format_func=lambda x: 'Sim' if x == 1 else 'Não')
-combustivel = st.selectbox('Combustível', ['alcool', 'diesel', 'eletrico', 'flex', 'gasolina', 'hibrido', 'outros'])
-direcao = st.selectbox('Tipo de Direção', ['Assistida', 'Elétrica', 'Hidráulica', 'Mecânica'])
-transmissao = st.selectbox('Tipo de Transmissão', ['Automatica', 'Manual', 'Semiautomatica'])
-
+with col2:
+    KM = st.number_input('Quilometragem (KM)', min_value=0.0, format="%f")
+    vidros_eletricos = st.selectbox('Vidros Elétricos', [0, 1], format_func=lambda x: 'Sim' if x == 1 else 'Não')
+    ar_condicionado = st.selectbox('Ar Condicionado', [0, 1], format_func=lambda x: 'Sim' if x == 1 else 'Não')
+    combustivel = st.selectbox('Combustível', ['alcool', 'diesel', 'eletrico', 'flex', 'gasolina', 'hibrido', 'outros'])
+    direcao = st.selectbox('Tipo de Direção', ['Assistida', 'Elétrica', 'Hidráulica', 'Mecânica'])
 
 
 # criando um dataframe com as entradas do usuário
@@ -79,17 +105,29 @@ input_data = pd.DataFrame({
     'Semiautomatica': [1 if transmissao == 'Semiautomatica' else 0]
 })
 
+#ajustando butão de prever preço
+st.markdown("""
+    <style>
+    .stButton button {
+        width: 100%;
+        padding: 15px;
+        font-size: 30px;
+    }    
+    .stAlert {
+        text-align: center;
+        font-size: 30px;
+    }
+    </style>
+""", unsafe_allow_html=True)
 
-
-def formatar_moeda_brl(valor):
-    valor_formatado = f"R$ {valor:,.2f}".replace('.', '#').replace(',', '.').replace('#', ',')
-    return valor_formatado
 # tratamentos da previsão
-if st.button('Prever Preço'):
-    try:
-        prediction = model.predict(input_data)
-        predicted_price = np.exp(prediction[0])  # Transformação inversa para obter o preço previsto
-        formatted_price = formatted_price = formatar_moeda_brl(predicted_price)
-        st.success(f"O preço previsto de venda é:   {formatted_price}")
-    except Exception as e:
-        st.error(f"Erro ao fazer a previsão: {e}")
+centered_button = st.empty()
+with centered_button.container():
+    if st.button('Prever Preço'):
+        try:
+            prediction = model.predict(input_data)
+            predicted_price = np.exp(prediction[0])  # Transformação inversa para obter o preço previsto
+            formatted_price = formatar_moeda_brl(predicted_price)
+            st.success(f"O preço previsto de venda é: {formatted_price}")
+        except Exception as e:
+            st.error(f"Erro ao fazer a previsão: {e}")
